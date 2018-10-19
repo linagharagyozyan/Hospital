50c657a9135bc26a0d7e7247191fd44064023116
linagharagyozyan <38084102+linagharagyozyan@users.noreply.github.com>
2018-10-19 10:52:37 +0400

    Update hospital.py

--- hospital.py
+++ hospital.py
@@ -1,90 +1,105 @@
+class HospitalProperties:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
 class Hospital:
     def __init__(self, name, address, phone):
+        self.__Jobhead = None
+        self.__Doctorhead = None
+        self.__Patienthead = None
         self.name = name
         self.address = address
         self.phone = phone
-        self.email = "{}{}{}{}".format("hospital", "_", self.name.lower(),"@gmail.com")
-        self.jobs = []
-        self.patients = {}
+        self.email = "{}{}{}{}".format("hospital","_",self.name.lower(),"@gmail.com")
 
-    def addPatient(self, doctor, patient):
-        if patient not in self.patients:
-            self.patients[doctor.name + " " + doctor.surname] = patient
-        return self.patients
+    def setHead(self, new_data):
+        self.__head = HospitalProperties(new_data)
+
+    def insert(self, data, new_data):
+        temp = self.__head
+        while temp.next is not None and temp != data:
+            temp = temp.next
+        new_data.next = temp.next
+        temp.next = new_data
 
     def getHospitalInfo(self):
-        print "Welcome to Hospital", self.name, "\n", "Contact details:","\n", "---------", "\n", "Address:", self.address,"\n", "Email:", self.email, "\n", "Phone:",self.phone
+        print("Welcome to Hospital", self.name, "\n""Contact details:""\n""---------","\n""Address:",self.address,"\n""Email: ",\
+              self.email,"\n""Phone: ",self.phone)
+
+    def getJobsInfo(self, data):
+        temp = self.__head
+        while temp.next is not None and temp != data:
+            temp = temp.next
+        print("\n",data.name)
+
+    def getDoctorInfo(self, data):
+        temp = self.__head
+        while temp.next is not None and temp != data:
+            temp = temp.next
+        print("\n""Name: ", data.name, "\n""Surname: ", data.surname, "\n""Email: ", data.email, \
+              "\n""Speciality: ", data.speciality, "\n""Phone: ", data.phone)
+
+    def getPatientInfo(self, data):
+        temp = self.__head
+        while temp.next is not None and temp != data:
+            temp = temp.next
+        print("\n""Patient info", "\n", "---------", "\n", "Name: ", data.name, "\n", "Surname: ", data.surname, "\n", "Email: ", \
+              data.email, "\n", "Disease: ", data.disease, "\n", "Status: ", data.status, "\n", "Phone: ", data.phone, "\n", "---------")
 
 class Job:
     def __init__(self, name):
         self.name = name
-        self.doctors = {}
-
-    def addDoctor(self, doctor, job):
-        if doctor not in self.doctors:
-            self.doctors[doctor.name + " " + doctor.surname] = job.name
-        return self.doctors
-
-    def getJobsInfo(self):
-        for key in self.doctors:
-            print "Doctor:", key, "\n", "Job:", self.doctors[key]
 
 class Doctor:
-    def __init__(self, name, surname, speciality, phone):
+    def __init__(self,name,surname,speciality,phone):
         self.name = name
-        self.surname = surname
-        self.email = "{}{}{}{}".format(self.name.lower(), "_", self.surname.lower(), "@gmail.com")
+        self.surname= surname
+        self.email = "{}{}{}{}".format(self.name.lower(),"_",self.surname.lower(),"@gmail.com")
         self.speciality = speciality
         self.phone = phone
-        self.patients = []
-
-    def getDoctorInfo(self):
-        print "Name: ", self.name, "\n", "Surname: ", self.surname, "\n", "Email: ", self.email,\
-            "\n", "Speciality: ", self.speciality, "\n", "Phone: ", self.phone
-
-    def addPatient(self, patient):
-            if patient not in self.patients:
-                return self.patients.append(patient)
-            else:
-                print patient.name, patient.surname, "is already in Dr.", self.name, self.surname, "'s list"
-
-    def printPatients(self):
-            for k in self.patients:
-                print "Patient info", "\n", "---------", "\n", "Name:", k.name, "\n", "Surname:", k.surname, "\n", "Email:", k.email, \
-                    "\n", "Disease:", k.disease, "\n", "Status: ", k.status, "\n", "Phone:", k.phone, "\n", "---------"
 
 class Patient:
-    def __init__(self, name, surname, disease, status, phone):
+    def __init__(self,name,surname,disease,status,phone):
         self.name = name
         self.surname = surname
-        self.email = "{}{}{}{}".format(self.name.lower(), "_", self.surname.lower(), "@gmail.com")
+        self.email = "{}{}{}{}".format(self.name.lower(),"_",self.surname.lower(),"@gmail.com")
         self.disease = disease
         self.status = status
         self.phone = phone
 
 def main():
-
     hospital = Hospital("Nairi", "Paronyan St., 21 Building", "+374-10-537500")
-    job1 = Job("Cardiologist")
-    doctor1 = Doctor("Armen", "Kirakosyan", "Cardiologist", "+374-77-887788")
-    patient1 = Patient("Tigran", "Khachatryan", "High blood pressure", "In progress", "+37477887799")
 
-    hospital.addPatient(doctor1, patient1.name)
+    job1 = Job("Cardiologist")
+    job2 = Job("Therapist")
+    hospital.setHead(job1)
+
+    doctor1 = Doctor("Armen","Kirakosyan","Cardiologist","+37477887788")
+    doctor2 = Doctor("Karen","Petrosyan","Cardiologist","+37491070097")
+    hospital.insert(job1, doctor1)
+    hospital.insert(job1, doctor2)
+
+    patient1 = Patient("Tigran","Khachatryan","High blood pressure","In progress","+37477887799")
+    patient2 = Patient("Gor","Nersisyan","Diabetes","In progress","+37495000000")
+
+    hospital.insert(doctor1, patient1)
+    hospital.insert(doctor1, patient2)
+
+    hospital.insert(hospital, job2)
+
     hospital.getHospitalInfo()
 
-    job1.addDoctor(doctor1, job1)
-    job1.getJobsInfo()
+    # hospital.getJobsInfo(job1)
+
+    hospital.getDoctorInfo(doctor1)
+    hospital.getPatientInfo(patient1)
+    hospital.getPatientInfo(patient2)
+    hospital.getDoctorInfo(doctor2)
 
 
-    doctor1.addPatient(patient1)
-    doctor1.getDoctorInfo()
-    doctor1.printPatients()
-    #doctor1.addPatient(patient1)
-
-main()
-
-
-#Lina Gharagyozyan, Garik Chilingaryan, Marianna Yesayan, Avet Dabaghyan, Varsik Poghosyan
+if __name__ == "__main__":
+    main()
 
 
 
