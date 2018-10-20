67be714585362e8fecdf8fcc5fe05b8fc5ea2c63
avet <avet0.dabaghyan@gmail.com>
2018-10-20 15:36:09 +0400

    Added insert and delete methods for Job, Doctor, Patient.

--- hospital.py
+++ hospital.py
@@ -46,6 +46,54 @@ class Hospital:
         newPatient = HospitalProperties(new_data)
         temp.next = newPatient
 
+    def insertJob(self, job, new_job):
+        temp = self.__Jobhead
+        while temp.next is not None and temp.next != job:
+            temp = temp.next
+
+        newJob = HospitalProperties(new_job)
+        newJob.next = temp.next
+        temp.next = newJob
+
+    def insertDoctor(self, doctor, new_doctor):
+        temp = self.__Doctorhead
+        while temp.next is not None and temp.next != doctor:
+            temp = temp.next
+
+        newDoctor = HospitalProperties(new_doctor)
+        newDoctor.next = temp.next
+        temp.next = newDoctor
+
+    def insertPatient(self, patient, new_patient):
+        temp = self.__Patienthead
+        while temp.next is not None and temp.next != patient:
+            temp = temp.next
+
+        newPatient = HospitalProperties(new_patient)
+        newPatient.next = temp.next
+        temp.next = newPatient
+
+    def deleteJob(self, job):
+        temp = self.__Jobhead
+        while temp.next is not None and temp.next != job:
+            temp = temp.next
+
+        temp.next = temp.next.next
+
+    def deleteDoctor(self, doctor):
+        temp = self.__Doctorhead
+        while temp.next is not None and temp.next != doctor:
+            temp = temp.next
+
+        temp.next = temp.next.next
+
+    def deletePatient(self, patient):
+        temp = self.__Patienthead
+        while temp.next is not None and temp.next != patient:
+            temp = temp.next
+
+        temp.next = temp.next.next
+
     def getHospitalInfo(self):
         print "Welcome to Hospital", self.name, "\n""Contact details:""\n""---------","\n""Address:",self.address,"\n""Email: ",\
               self.email,"\n""Phone: ",self.phone
@@ -98,16 +146,22 @@ def main():
     hospital.setJobHead(job1)
     job2 = Job("Therapist")
     hospital.appendJob(job2)
+    job3 = Job("Surgeon")
+    #insert job1, job3
 
     doctor1 = Doctor("Armen","Kirakosyan","Cardiologist","+37477887788")
     hospital.setDoctorHead(doctor1)
     doctor2 = Doctor("Karen","Petrosyan","Cardiologist","+37491070097")
     hospital.appendDoctor(doctor2)
+    doctor3 = Doctor("Petros", "Karapetyan","Surgeon","+37498288888")
+    #insert doctor1, doctor3
 
     patient1 = Patient("Tigran","Khachatryan","High blood pressure","In progress","+37477887799")
     hospital.setPatientHead(patient1)
     patient2 = Patient("Gor","Nersisyan","Diabetes","In progress","+37495000000")
     hospital.appendPatient(patient2)
+    patient3 = Patient("Poghos", "Kaputikyan", "Broken toe", "In progress", "+374394822")
+    #insert patient1, patient3
 
     hospital.getHospitalInfo()
 
